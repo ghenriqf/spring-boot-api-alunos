c18d90708abd6cfd4bb3ededff0c05daeafdba6f
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-08-29 16:28:28 -0300

    initial commit

---
+++ src/main/java/com/ghenriqf/spring_boot_api_alunos/SpringBootApiAlunosApplication.java
@@ -0,0 +1,13 @@
+package com.ghenriqf.spring_boot_api_alunos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiAlunosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApiAlunosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ghenriqf/spring_boot_api_alunos/SpringBootApiAlunosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghenriqf.spring_boot_api_alunos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApiAlunosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
