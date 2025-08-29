216a4a3ab8e7e354a9f47c7325450d99fb26f255
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-08-29 16:42:06 -0300

    feat: implementar controller inicial

---
+++ src/main/java/com/ghenriqf/spring_boot_api_alunos/controller/AlunoController.java
@@ -0,0 +1,11 @@
+package com.ghenriqf.spring_boot_api_alunos.controller;
+
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/alunos")
+public class AlunoController {
+
+}
