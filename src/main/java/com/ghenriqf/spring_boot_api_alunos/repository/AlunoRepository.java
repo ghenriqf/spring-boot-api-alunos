6b69f57ef75798560544532bc1a7d3e55d7e876f
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-09-01 16:36:47 -0300

    feat: implementar repository

---
+++ src/main/java/com/ghenriqf/spring_boot_api_alunos/repository/AlunoRepository.java
@@ -0,0 +1,9 @@
+package com.ghenriqf.spring_boot_api_alunos.repository;
+
+import com.ghenriqf.spring_boot_api_alunos.domain.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AlunoRepository extends JpaRepository<Aluno, UUID> {
+}
