6240cce70529df4ca697e3c74941701253f06287
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-09-17 18:43:29 -0300

    feat: implementar pseudo save no service

--- src/main/java/com/ghenriqf/spring_boot_api_alunos/service/AlunoService.java
+++ src/main/java/com/ghenriqf/spring_boot_api_alunos/service/AlunoService.java
@@ -1,5 +1,33 @@
 package com.ghenriqf.spring_boot_api_alunos.service;
 
-public class AlunoService {
+import com.ghenriqf.spring_boot_api_alunos.dto.AlunoRequestDTO;
+import com.ghenriqf.spring_boot_api_alunos.dto.AlunoResponseDTO;
+import com.ghenriqf.spring_boot_api_alunos.mapper.AlunoMapper;
+import com.ghenriqf.spring_boot_api_alunos.model.Aluno;
+import com.ghenriqf.spring_boot_api_alunos.repository.AlunoRepository;
 
+public class AlunoService {
+    private final AlunoRepository alunoRepository;
+    private final AlunoMapper alunoMapper;
+
+    public AlunoService(AlunoRepository alunoRepository, AlunoMapper alunoMapper) {
+        this.alunoRepository = alunoRepository;
+        this.alunoMapper = alunoMapper;
+    }
+
+
+
+    public AlunoResponseDTO salvarAluno(AlunoRequestDTO alunoRequestDTO) {
+//        if (alunoRepository.existsByEmail(alunoRequestDTO.email())) {
+//
+//        }
+
+//        if (alunoRepository.existsByCpf(alunoRequestDTO.cpf())) {
+//
+//        }
+        Aluno aluno = alunoMapper.toEntity(alunoRequestDTO);
+        Aluno alunoSalvo = alunoRepository.save(aluno);
+
+        return alunoMapper.toResponse(aluno);
+   }
 }
