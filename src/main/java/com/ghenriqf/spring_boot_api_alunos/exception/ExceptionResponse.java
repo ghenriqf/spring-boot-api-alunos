a85ba92d69c9e1c04584a262000864a079d4bf93
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-09-18 11:08:39 -0300

    feat: implementar padrão de resposta para as exeções

---
+++ src/main/java/com/ghenriqf/spring_boot_api_alunos/exception/ExceptionResponse.java
@@ -0,0 +1,27 @@
+package com.ghenriqf.spring_boot_api_alunos.exception;
+
+import java.time.LocalDateTime;
+
+public class ExceptionResponse {
+    private String mensagem;
+    private int status;
+    LocalDateTime timestamp;
+
+    public ExceptionResponse(String mensagem, int status) {
+        this.mensagem = mensagem;
+        this.status = status;
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+}
