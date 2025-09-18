db4095518bfcb9982a5f07f008aec7b8ca8198a4
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-09-18 13:57:53 -0300

    feat: implementar metodo de salvar aluno service

--- src/main/java/com/ghenriqf/spring_boot_api_alunos/service/AlunoService.java
+++ src/main/java/com/ghenriqf/spring_boot_api_alunos/service/AlunoService.java
@@ -2,6 +2,7 @@ package com.ghenriqf.spring_boot_api_alunos.service;
 
 import com.ghenriqf.spring_boot_api_alunos.dto.AlunoRequestDTO;
 import com.ghenriqf.spring_boot_api_alunos.dto.AlunoResponseDTO;
+import com.ghenriqf.spring_boot_api_alunos.exception.RecursoDuplicadoException;
 import com.ghenriqf.spring_boot_api_alunos.mapper.AlunoMapper;
 import com.ghenriqf.spring_boot_api_alunos.model.Aluno;
 import com.ghenriqf.spring_boot_api_alunos.repository.AlunoRepository;
@@ -15,19 +16,17 @@ public class AlunoService {
         this.alunoMapper = alunoMapper;
     }
 
-
-
     public AlunoResponseDTO salvarAluno(AlunoRequestDTO alunoRequestDTO) {
-//        if (alunoRepository.existsByEmail(alunoRequestDTO.email())) {
-//
-//        }
-
-//        if (alunoRepository.existsByCpf(alunoRequestDTO.cpf())) {
-//
-//        }
+        if (alunoRepository.existsByEmail(alunoRequestDTO.email())) {
+            throw new RecursoDuplicadoException("email já registrado: " + alunoRequestDTO.email());
+        }
+        if (alunoRepository.existsByCpf(alunoRequestDTO.cpf())) {
+            throw new RecursoDuplicadoException("cpf já registrado: " + alunoRequestDTO.cpf());
+        }
         Aluno aluno = alunoMapper.toEntity(alunoRequestDTO);
         Aluno alunoSalvo = alunoRepository.save(aluno);
 
-        return alunoMapper.toResponse(aluno);
+        return alunoMapper.toResponse(alunoSalvo);
    }
+
 }
