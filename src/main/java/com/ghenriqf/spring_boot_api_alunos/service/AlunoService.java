312d8e54789007cc6a48c18a1054995f4e6e2da1
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-09-23 16:58:14 -0300

    feat: implementar metodo de find by id no service

--- src/main/java/com/ghenriqf/spring_boot_api_alunos/service/AlunoService.java
+++ src/main/java/com/ghenriqf/spring_boot_api_alunos/service/AlunoService.java
@@ -3,10 +3,16 @@ package com.ghenriqf.spring_boot_api_alunos.service;
 import com.ghenriqf.spring_boot_api_alunos.dto.AlunoRequestDTO;
 import com.ghenriqf.spring_boot_api_alunos.dto.AlunoResponseDTO;
 import com.ghenriqf.spring_boot_api_alunos.exception.RecursoDuplicadoException;
+import com.ghenriqf.spring_boot_api_alunos.exception.RecursoNotFoundException;
 import com.ghenriqf.spring_boot_api_alunos.mapper.AlunoMapper;
 import com.ghenriqf.spring_boot_api_alunos.model.Aluno;
 import com.ghenriqf.spring_boot_api_alunos.repository.AlunoRepository;
 import org.springframework.stereotype.Service;
+
+import javax.swing.*;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class AlunoService {
@@ -16,6 +22,10 @@ public class AlunoService {
     public AlunoService(AlunoRepository alunoRepository, AlunoMapper alunoMapper) {
         this.alunoRepository = alunoRepository;
         this.alunoMapper = alunoMapper;
+    }
+    public AlunoResponseDTO buscarAluno(UUID id) {
+
+        return alunoRepository.findById(id).map(alunoMapper::toResponse).orElseThrow(() -> new RecursoNotFoundException(id));
     }
 
     public AlunoResponseDTO salvarAluno(AlunoRequestDTO alunoRequestDTO) {
