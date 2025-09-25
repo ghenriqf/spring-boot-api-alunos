f2c71c4cfd0a546f3392ab3dbc45d7aba8539eb1
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-09-25 16:57:22 -0300

    refactor: adicionar anotação no repository

--- src/main/java/com/ghenriqf/spring_boot_api_alunos/repository/AlunoRepository.java
+++ src/main/java/com/ghenriqf/spring_boot_api_alunos/repository/AlunoRepository.java
@@ -2,9 +2,11 @@ package com.ghenriqf.spring_boot_api_alunos.repository;
 
 import com.ghenriqf.spring_boot_api_alunos.model.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface AlunoRepository extends JpaRepository<Aluno, UUID> {
     boolean existsByEmail(String email);
     boolean existsByCpf(String cpf);
