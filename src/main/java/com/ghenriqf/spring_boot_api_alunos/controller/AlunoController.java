f915b8c3ce8a1cabe938671c2124fbf77ef39394
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-09-25 16:56:49 -0300

    feat: criar metodo inicial de busca no controller

--- src/main/java/com/ghenriqf/spring_boot_api_alunos/controller/AlunoController.java
+++ src/main/java/com/ghenriqf/spring_boot_api_alunos/controller/AlunoController.java
@@ -1,11 +1,24 @@
 package com.ghenriqf.spring_boot_api_alunos.controller;
 
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ghenriqf.spring_boot_api_alunos.dto.AlunoResponseDTO;
+import com.ghenriqf.spring_boot_api_alunos.repository.AlunoRepository;
+import com.ghenriqf.spring_boot_api_alunos.service.AlunoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/alunos")
 public class AlunoController {
 
+    private final AlunoService alunoService;
+
+    public AlunoController(AlunoService alunoService) {
+        this.alunoService = alunoService;
+    }
+
+    @GetMapping("/{id}")
+    public AlunoResponseDTO buscarAluno(@PathVariable UUID id) {
+        return alunoService.buscarAluno(id);
+    }
 }
