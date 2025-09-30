1ea01f67afe7df766042597c289d4185b88bea11
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-09-30 11:55:29 -0300

    feat(controller): implementar metodos deletar, listar e criar

--- src/main/java/com/ghenriqf/spring_boot_api_alunos/controller/AlunoController.java
+++ src/main/java/com/ghenriqf/spring_boot_api_alunos/controller/AlunoController.java
@@ -1,10 +1,14 @@
 package com.ghenriqf.spring_boot_api_alunos.controller;
 
+import com.ghenriqf.spring_boot_api_alunos.dto.AlunoRequestDTO;
 import com.ghenriqf.spring_boot_api_alunos.dto.AlunoResponseDTO;
 import com.ghenriqf.spring_boot_api_alunos.repository.AlunoRepository;
 import com.ghenriqf.spring_boot_api_alunos.service.AlunoService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -17,8 +21,23 @@ public class AlunoController {
         this.alunoService = alunoService;
     }
 
+    @PostMapping
+    public AlunoResponseDTO criarAluno(@RequestBody @Valid @NotNull AlunoRequestDTO aluno) {
+        return alunoService.criarAluno(aluno);
+    }
+
     @GetMapping("/{id}")
     public AlunoResponseDTO buscarAluno(@PathVariable UUID id) {
         return alunoService.buscarAluno(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public AlunoResponseDTO deletarAluno(@PathVariable UUID id) {
+        return alunoService.deletarAluno(id);
+    }
+
+    @GetMapping
+    public List<AlunoResponseDTO> listarAlunos() {
+        return alunoService.listarAlunos();
     }
 }
